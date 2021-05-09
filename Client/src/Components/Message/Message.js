b6c8aed6c879ea728d06341ce1761495eb3fedf0
Ponz35 <romain35rp@gmail.com>
2021-05-09 15:56:24 +0200

    correction gravatar

--- Client/src/Components/Message/Message.js
+++ Client/src/Components/Message/Message.js
@@ -87,7 +87,7 @@ function Message({ message, i, fetchMessages }) {
 
   return (
     <div id="messageDiv" key={i} onMouseOver={lookOwner}>
-      <Gravatar email={currentUser.user.email}/>
+      <Gravatar email={user.email}/>
       <div
         style={{ display: "flex", flexDirection: "column", padding: "10px" }}
       >
